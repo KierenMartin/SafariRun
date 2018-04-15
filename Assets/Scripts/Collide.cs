a2d11f4d932ab87ede6d575398ddc65eaa0d8a07
Finella Michael <finellamichael@gmail.com>
2018-04-15 12:04:57 +1000

    created Collide script
    It is attached to the Rhino and detects a collision with the log and the poacher which is indicated by a message in the console window

---
+++ Assets/Scripts/Collide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour
+{
+
+	void OnCollisionEnter2D (Collision2D col)
+    {
+		if(col.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Collision");
+        }
+
+        if(col.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Game Over");
+
+        }
+    }
+	
+	
+}
