bb6cc984684b5ffe8a904c4753d18f3fd306b560
AlyMorris <36952578+AlyMorris@users.noreply.github.com>
2018-05-09 05:25:17 +1000

    Pause Menu

---
+++ Assets/Scenes/Reload.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI()
+    {
+		if (GUI.Button (new Rect (Screen.width - ((Screen.width / 60)) - (Screen.width / 20), Screen.height / 60, Screen.width / 20, Screen.width / 20), "Rt"))
+	    {
+             Application.LoadLevel(Application.loadedLevel); 
+    
+        }  
+    }
+    
+        
+}    
+
